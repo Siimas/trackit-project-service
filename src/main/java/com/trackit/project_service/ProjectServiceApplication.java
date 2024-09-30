ebb06d51f64933ac8e5376befab53e652405a8b6
Siimas <simaomarques20@gmail.com>
2024-09-30 22:39:21 +0100

    first commit

---
+++ src/main/java/com/trackit/project_service/ProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trackit.project_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trackit/project_service/ProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackit.project_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
